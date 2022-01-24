11757845f98e7c1ac119ac54eda6d9eefd270018
jonnn13 <jon.s.ayuso@gmail.com>
2022-01-24 13:21:51 +0100

    ejercicios del 1 al 7 del trabajo personal

---
+++ Trabajo_Personal.R
@@ -0,0 +1,49 @@
+install.packages("nycflights13")
+library(nycflights13)
+vuelos= nycflights13::flights
+vuelos_buenos= vuelos[complete.cases(vuelos),]
+?flights
+# Observamos que los datos de la arr_delay estan en minutos 
+
+retrasos= vuelos[which(vuelos$arr_delay> 60), ]
+a=dim(retrasos)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que llegaron con retraso"))
+
+vSF= vuelos[vuelos$dest== "SFO" | vuelos$dest== "OAK",]
+a=dim(vSF)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que volaron hacia San Francisco"))
+
+vOp= vuelos[vuelos$carrier == "UA" | vuelos$carrier == "AA",]
+a=dim(vOp)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que volaron operados por United American (UA) o por American Airlines (AA)"))
+
+
+vPrim= vuelos[vuelos$month=="4" | vuelos$month=="5" | vuelos$month=="6",]
+a=dim(vPrim)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que volaron en los meses de primavera"))
+
+
+retrasados2= vuelos[which(vuelos$arr_delay> 60 & vuelos$dep_delay< 60), ]
+a=dim(retrasados2)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que llegaron con más de una hora de retraso y salieron con menos de una hora de retraso"))
+
+
+
+retrasados3= vuelos[which(vuelos$arr_delay< 30 & vuelos$dep_delay> 60), ]
+a=dim(retrasados3)
+a[1]
+print(paste0("Hay ", a[1], " vuelos que llegaron con menos de 30 minutos de retraso y salieron con más de una hora de retraso"))
+
+
+
+vnocturnos= vuelos[which(vuelos$hour>=0 & vuelos$hour<=7 ),]
+a=dim(vnocturnos)
+a[1]
+print(paste0("Hay ", a[1], " vuelos nocturnos"))
+
+
