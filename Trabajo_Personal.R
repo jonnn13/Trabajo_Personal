df5d497deeb97e462c6d8a152ea60b4d141d3973
jonnn13 <jon.s.ayuso@gmail.com>
2022-01-30 23:06:45 +0100

    Tranajo personal terminado

--- Trabajo_Personal.R
+++ Trabajo_Personal.R
@@ -72,25 +72,36 @@ v_rap=vuelos[order(vuelos$velocidad, na.last = F),]
 tail(v_rap)
 
 #Ejercicio 12
-v_trayecto=vuelos_buenos[order(vuelos_buenos$distance, decreasing = T),]
+v_trayecto=vuelos[order(vuelos$distance, decreasing = T),]
 head(v_trayecto)
 
 # E13
+v_trayecto=vuelos[order(vuelos$distance, decreasing = F),]
 tail(v_trayecto)
-##### CAMBIARRR ESTA MALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+
 
 
 
 
 # E14
-vuelos$dep_time_min= (vuelos$dep_time %/%100*60 + vuelos$dep_time %%100)
+vuelos$dep_time_min= (vuelos$dep_time %/%100*60 + vuelos$dep_time %%100) 
 vuelos$sched_dep_time_min= (vuelos$sched_dep_time %/%100*60 + vuelos$sched_dep_time %%100)
+
+
+###vuelos$dep_time_min2= (vuelos$dep_time %/%100*60 + vuelos$dep_time %%100) %% 1440
+####vuelos$sched_dep_time_min2= (vuelos$sched_dep_time %/%100*60 + vuelos$sched_dep_time %%100) %% 1440
+
+
 
 # E15
 vuelos$relacion= (vuelos$dep_time_min - vuelos$sched_dep_time_min - (vuelos$dep_delay))
 table(vuelos$relacion)
-##explicar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+print("Podemos correctamente la relaciones entre estas varibales en 327314 vuelos ya que en la resta obtenemos cero. Sin embrago, en 1207 vuelos no podemos observar correctamente esa relación ya que la resta nos da 1440, esto se debe a que el retraso al que ha ido sometido el vuelo hace que el este despege en un día diferente, es decir, que durante ese cambio de horario trascurre la media noche.")
 
+
+# Como podemos observar existen 327314 vuelos en los cuales coincide que la diferencia entre (dep_time, hora a la que sale 
+# realmente) menos (sched_dep_time, que es la hora a la que tenia programado salir) coincide con dep_delay que es el retraso 
+# que llevan. En 1207 vuelos existe una diferencia de 1440, que se debe a que estos vuelos parten en media noche
 
 # E16
 cancelado_por_dia <- 
@@ -101,6 +112,9 @@ cancelado_por_dia <-
     cancelado_num = sum(cancelado),
     flights_num = n(),
   )
+
+#cancelado=(is.na(arr_delay) | is.na(dep_delay)))--> nos dice que los cancelados son los que tienen valores NA en arr_delay y dep_delay
+ 
 
 ggplot(cancelado_por_dia) +
   geom_point(aes(x = flights_num, y = cancelado_num))
@@ -121,11 +135,30 @@ cancelados_y_retrasos <-
 ggplot(cancelados_y_retrasos) +
   geom_point(aes(x = media_dep_delay, y = prop_cancelado))
 
-print("Podemos observar que hay una fuerte tendencia lineal de los datos por lo que los vuelos cancelado y los retrasados están relacionados")
+ggplot(cancelados_y_retrasos) +
+  geom_point(aes(x = media_arr_delay, y = prop_cancelado))
+
+print("Podemos observar que hay una fuerte tendencia lineal de los datos en ambas gráficas por lo que la proporción de vuelos cancelados y los retrasados en la salida y en la llegada están relacionados")
 
 
 
-# E18
+# E18 MIRAR CON JAVI
+cancelados_y_retrasos_A <- 
+  vuelos %>%
+  mutate(cancelado = (is.na(arr_delay) | is.na(dep_delay))) %>%
+  group_by(origin, dest) %>% 
+  summarise(
+    prop_cancelado = mean(cancelado),
+    media_dep_delay = mean(dep_delay, na.rm = TRUE),
+    media_arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) %>%
+  ungroup()
+
+ggplot(cancelados_y_retrasos_A) +
+  geom_point(aes(x = media_dep_delay, y = prop_cancelado))
+
+ggplot(cancelados_y_retrasos_A) +
+  geom_point(aes(x = media_arr_delay, y = prop_cancelado))
 
 
 
@@ -142,9 +175,81 @@ vuelos %>%
 print("La F9 es la que más se retrasa tanto en salidas como en llegadas")
 
 
-# E20
+# E20 ************************************************************************
 vuelos %>%
   group_by(hour) %>%
-  summarise(arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
-  arrange(arr_delay)
+  summarise(dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
+  arrange(dep_delay)
 print("Para evitar los retrasos en la salida la mejor hora para volar son las 5 de la mañana")
+
+
+# E21
+make_datetime_100 <- function(year, month, day, time) 
+{ 
+  make_datetime(year, month, day, time %/% 100, time %% 100) 
+  }
+
+flights_dt <- vuelos %>% 
+  filter(!is.na(dep_time), !is.na(arr_time)) %>% 
+  mutate( dep_time = make_datetime_100(year, month, day, dep_time), 
+          arr_time = make_datetime_100(year, month, day, arr_time), 
+          sched_dep_time = make_datetime_100(year, month, day, sched_dep_time), 
+          sched_arr_time = make_datetime_100(year, month, day, sched_arr_time) ) %>% 
+  select(origin, dest, ends_with("delay"), ends_with("time"))
+
+flights_dt2<- flights_dt %>%
+  mutate(dow = wday(sched_dep_time)) %>%
+  group_by(dow) %>%
+  summarise(
+    dep_delay = mean(dep_delay),
+    arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) %>%
+  print(n = Inf)
+
+flights_dt %>%
+  mutate(wday = wday(dep_time, label = TRUE)) %>% #porque de dep_time
+  group_by(wday) %>% 
+  summarize(ave_dep_delay = mean(dep_delay, na.rm = TRUE)) %>% 
+  ggplot(aes(x = wday, y = ave_dep_delay)) + 
+  geom_bar(stat = "identity") 
+print("El sábado es el día de la semana que menos retrasos en la salida hay")
+
+
+# E22
+retraso_acum<- vuelos %>% 
+  filter(arr_delay > 0) %>% 
+  group_by(dest) %>% 
+  summarise(total_delay= sum(arr_delay))%>% 
+  arrange(total_delay)
+
+
+
+
+
+  
+  
+# E23   ?????
+
+
+totalretra= sum(retraso_acum$total_delay)
+retraso_acum2<- vuelos %>% 
+  filter(arr_delay > 0) %>% 
+  group_by(dest) %>% 
+  summarise(total_delay= sum(arr_delay),
+            prop_total_delay= total_delay / totalretra)%>% 
+  arrange(prop_total_delay)
+view(totalretra) 
+
+
+
+  
+# 24
+
+flights_dt %>% 
+  mutate(sched_dep_hour = hour(sched_dep_time)) %>% 
+  group_by(sched_dep_hour) %>% 
+  summarise(dep_delay = mean(dep_delay)) %>% 
+  ggplot(aes(y = dep_delay, x = sched_dep_hour)) + 
+  geom_point() + geom_smooth()
+
+
